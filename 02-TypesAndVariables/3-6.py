39d21e69868ca6f7e7dd1eab3b5dab667193e8c4
Bartosz Koziol <237382@student.uek.krakow.pl>
2024-10-08 11:40:02 +0200

    sekcja 3 skończona

---
+++ 02-TypesAndVariables/3-6.py
@@ -0,0 +1,14 @@
+import math   
+#d = 3.57 × √h
+#diagonal = math.sqrt(a**2+b**2)
+
+
+h1 = 1.9
+h2 = 20
+#d=3.57*math.sqrt(h) 
+
+d1=3.57*math.sqrt(h1)
+d2=3.57*math.sqrt(h2)
+
+print(d1)
+print(d2)
